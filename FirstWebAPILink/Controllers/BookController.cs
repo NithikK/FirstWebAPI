a94c8da3085cff50747375e843fbf1cdc6125646
NithikK <nithik.k@gavstech.com>
2023-09-21 14:01:59 +0530

    Included Book and BookController

---
+++ FirstWebAPILink/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using FirstWebAPILink.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPILink.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        static List<Book> books = new List<Book>();
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            books.Add(new Book() { Id = 1, Name = "C# Programming", AuthorName = "Rahul" });
+            books.Add(new Book() { Id = 2, Name = "Java Programming", AuthorName = "Rahul" });
+            books.Add(new Book() { Id = 3, Name = "Python Programming", AuthorName = "Rahul" });
+            books.Add(new Book() { Id = 4, Name = "C++ Programming", AuthorName = "Rahul" });
+            books.Add(new Book() { Id = 5, Name = "C Programming", AuthorName = "Rahul" });
+            return books;
+        }
+
+        [HttpPost]
+        public int AddBook(Book book)
+        {
+            books.Add(book);
+            return 1;
+        }
+    }
+}
---
+++ FirstWebAPILink/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstWebAPILink.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
