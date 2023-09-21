bf3ee5b5071cd19b21f9a9ca34ad86808532d8f6
Nithik K <nithik.k@gavstech.com>
2023-09-21 11:58:12 +0530

    FirstCommit

---
+++ FirstWebApiLink/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace FirstWebApiLink.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        //api/Calculator/Add?x=10&y=10
+        [HttpGet("/api/Calculator/Add")]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        //api/Calculator/Substract?x=10&y=10
+        [HttpGet("/api/Substract")]
+        public int Substract(int x, int y)
+        {
+            return x - y;
+        }
+        //api/Calculator/Multiply?x=10&y=10
+        [HttpGet("/api/Multiply")]//to have multiply get give parameters
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        //api/Calculator/Divide?x=10&y=10
+        [HttpGet("/api/Divide")]
+        public double Divide(int x, int y)
+        {
+            return x / y;
+        }
+        //api/Calculator/Power?x=10&y=10
+        [HttpGet("/api/Power")]
+        public double Power(int x, int y)
+        {
+            return Math.Pow(x, y);
+        }
+    }
+}
---
+++ FirstWebApiLink/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApiLink.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        //api/Calculator/Add?x=10&y=10
+        [HttpGet("/api/Demo/Add")]
+        public int Add(int x, int y)
+        {
+            return x + y +50000;
+        }
+    }
+}
